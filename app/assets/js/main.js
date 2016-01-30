f47bd6393393b08b0b324e98dbc614853c375f82
Christopher Wright <cwright814@gmail.com>
2016-01-30 03:31:29 -0500

    Bug fixes

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -61,89 +61,91 @@ function tick(event) {
 }
 
 var player = {
-  ground: true,
-  pos: {
-    x: w/2,
-    y: h/2},
-  speed: {
-    x: 0,
-    y: 0},
-  width: 32,
-  height: 64,
-  land: playerLand,
-  setbounds: setBounds,
-  updatepos: updatePos,
-  initsensor: initSensor,
-  update: playerUpdate
+    width: 32,
+    height: 64,
+    pos: {
+        x: w/2,
+        y: h/2},
+    speed: {
+        x: 0,
+        y: 0},
+    ground: true,
+    land: playerLand,
+    setbounds: setBounds,
+    updatepos: updatePos,
+    initsensor: initSensor,
+    update: playerUpdate
 };
 player.initsensor("right", 4, player.height-8, player.width/2, 0);
 player.initsensor("left", 4, player.height-8, -player.width/2, 0);
-player.initsensor("top", player.width, 4, 0, player.height/2);
-player.initsensor("bottom", player.width, 4, 0, -player.height/2);
+player.initsensor("bottom", player.width, 4, 0, player.height/2);
+player.initsensor("top", player.width, 4, 0, -player.height/2);
 
-if (!player.ground && player.speed.y < 0 && player.sensor.bottom.colliding())
-  player.land();
+if (!player.ground && player.speed.y > 0 && player.sensor.bottom.colliding())
+    player.land();
 
 function playerLand() {
-  player.ground = true;
-  player.speed.y = 0;
-  // Add repositioning logic here
+    player.ground = true;
+    player.speed.y = 0;
+    // Add repositioning logic here
 }
 
 function playerUpdate() {
-  this.updatepos();
-  for (var i = 0; i < this.sensor.length; i++)
-    this.sensor[i].updatepos();
+    this.updatepos();
+    for (var i = 0; i < this.sensor.length; i++)
+        this.sensor[i].updatepos();
 }
 
 function initSensor(label, width, height, offsetX, offsetY) {
-    if (typeof(this.sensor) === undefined)
-    this.sensor = {};
-  this.sensor[label] = {
-    pos: {
-      x: this.pos.x,
-      y: this.pos.y
-    },
-    offset: {
-      x: offsetX,
-      y: offsetY
-    },
-    parent: this,
-    setbounds: setBounds,
-    updatepos: updatePos
-  };
+    if (typeof(this.sensor) === 'undefined')
+        this.sensor = {};
+    this.sensor[label] = {
+        width: width,
+        height: height,
+        pos: {
+            x: this.pos.x,
+            y: this.pos.y
+        },
+        offset: {
+            x: offsetX,
+            y: offsetY
+        },
+        parent: this,
+        setbounds: setBounds,
+        updatepos: updatePos
+    };
 }
 
 function updatePos(delta) {
-  if (typeof(this.offset) !== undefined) {
-    this.pos.x = parent.pos.x + this.offset.x;
-    this.pos.y = parent.pos.y + this.offset.y;
-  } else if (typeof(this.speed) !== undefined) {
-    this.pos.x += this.speed.x * delta;
-    this.pos.y += this.speed.y * delta;
-  }
+    if (typeof(this.offset) !== 'undefined') {
+        this.pos.x = parent.pos.x + this.offset.x;
+        this.pos.y = parent.pos.y + this.offset.y;
+    } else if (typeof(this.speed) !== 'undefined') {
+        this.pos.x += this.speed.x * delta;
+        this.pos.y += this.speed.y * delta;
+    }
 }
 
 function setBounds() {
-  this.bound = {
-    right: this.pos.x + this.width/2,
-    left: this.pos.x - this.width/2,
-    top: this.pos.y + this.height/2,
-    bottom: this.pos.y - this.height/2
-  };
+    this.bound = {
+        right: this.pos.x + this.width/2,
+        left: this.pos.x - this.width/2,
+        bottom: this.pos.y + this.height/2,
+        top: this.pos.y - this.height/2
+    };
 }
 
 function colliding() {
-  this.setbounds();
-  for (var i = 0; i < objects.length; i++) {
-    var object = objects[i];
-    if (this.bound.right > object.bound.left &&
-        this.bound.left < object.bound.right &&
-        this.bound.top > object.bound.bottom &&
-        this.bound.bottom < object.bound.top)
-      return true;
-  }
-  return false;
+    this.setbounds();
+    for (var i = 0; i < objects.length; i++) {
+        var object = objects[i];
+        if (this.bound.right > object.bound.left &&
+            this.bound.left < object.bound.right &&
+            this.bound.bottom > object.bound.top &&
+            this.bound.top < object.bound.bottom)
+            return true;
+    }
+    return false;
 }
 
 function keyPressed(event) {
